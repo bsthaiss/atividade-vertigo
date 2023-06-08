f8d1fa196c65b68dc817b5f1cd16dc0f44f89966
Thais Barbosa <thaisbs1405@gmail.com>
2023-06-08 19:25:33 -0300

    Adicionei uma funcionalidade

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -1,6 +1,17 @@
+import { useState } from 'react'
 import Eye from '../assets/eye-off.svg'
 
 export function Principal() {
+    const [email, setEmail] = useState(" ")
+    const [senha, setSenha] = useState(" ")
+
+    function handleMostrar() {
+        alert(`
+            E-mail digitado: ${email}
+            Senha digitada: ${senha}
+        `)
+    }
+
     return (
         <section>
             <div className="texts">
@@ -11,17 +22,17 @@ export function Principal() {
             <form action="">
                 <div className="input-box">
                     <label htmlFor="">E-mail</label>
-                    <input type="email" id="email" placeholder="Digite seu e-mail" />
+                    <input type="email" id="email" placeholder="Digite seu e-mail" onChange={(e) => setEmail(e.target.value)}/>
                 </div>
 
                 <div className="input-box">
                     <label htmlFor="">Senha<a href="#">Esqueceu a senha?</a></label>
-                    <input type="password" id="senha" placeholder="Digite sua senha" />
+                    <input type="password" id="senha" placeholder="Digite sua senha" onChange={(e) => setSenha(e.target.value)}/>
                     <img src={Eye} alt="Mostrar/esconder senha" />
                 </div>
 
                 <div className="button">
-                    <button type="submit">Entrar</button>
+                    <button type="submit" onClick={handleMostrar}>Entrar</button>
                 </div>
 
                 <div className="create-account">
