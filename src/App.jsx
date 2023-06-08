33c74f6acf94eda112382754f78675c7c92ad0ee
Thais Barbosa <thaisbs1405@gmail.com>
2023-06-08 19:08:53 -0300

    Atualizei os arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from './components/Header'
 import { Principal } from './components/Principal'
 import { Image } from './components/Image'
+import './index.css'
 
 export function App() {
   return (
